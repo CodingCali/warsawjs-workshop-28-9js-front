e9abf9d9a55489ab9f806ec989d25aae2d0284cd
seapage <kontakt@seapage.pl>
2019-01-11 00:22:07 +0100

    stage-5 replace every image

--- public/sw.js
+++ public/sw.js
@@ -5,11 +5,18 @@ var actualVersion = '1'
 self.addEventListener('fetch',function (event) {
 
 
-    event.respondWith(
-        fetch(`/image/nointernet.gif`).then(response=>{
 
-            return response
-        })
-    )
+    if (event.request.url.endsWith('.jpg')
+        ||event.request.url.endsWith('.jpeg')
+        ||event.request.url.endsWith('.gif')
+        ||event.request.url.endsWith('.png')
+    ){
+        event.respondWith(
+            fetch(`/image/nointernet.gif`).then(response=>{
+
+                return response
+            })
+        )
+    }
 
 })
