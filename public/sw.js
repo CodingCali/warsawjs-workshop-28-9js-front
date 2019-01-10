c90abc86dd5295679e9fdaaf12133ed582fbd4ea
seapage <kontakt@seapage.pl>
2019-01-11 00:30:12 +0100

    stage-7 first-app-offline

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,20 @@
 
 var actualVersion = '1'
 
+self.addEventListener('install', function(event) {
+    caches.open('storage-'+actualVersion).then((cacheOpened) => {
+
+        return cacheOpened.addAll([
+            '/image/nointernet.gif'
+        ])}
+    )
+});
 
 self.addEventListener('fetch',function (event) {
 
+        event.respondWith(
+            caches.match('/image/nointernet.gif')
+        )
 
 
     // if (event.request.url.endsWith('.jpg')
