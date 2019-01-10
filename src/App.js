2046cf1cbac9dcc77fb298ea995e6221cef2611c
seapage <kontakt@seapage.pl>
2019-01-10 23:59:13 +0100

    online/offline recognaction

--- src/App.js
+++ src/App.js
@@ -6,9 +6,50 @@ import PostFavList from './PostFavList'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    internetOnline: true
+  }
+
+  componentDidMount() {
+
+
+    if(!navigator.onLine){
+      this.setState({
+        ...this.state,
+        internetOnline: false
+      })
+    }
+
+    window.addEventListener('offline', (e) => {
+      this.setState({
+        ...this.state,
+        internetOnline: false
+      })
+    });
+
+    window.addEventListener('online', (e) => {
+      this.setState({
+        ...this.state,
+        internetOnline: true
+      })
+    });
+
+  }
+
+
   render() {
     return (
       <div className="App">
+        <div id="warnings">
+          {(!this.state.internetOnline)?(
+              <div id={"internet_warning"}>
+                You haven't got connection
+              </div>
+          ):("")}
+        </div>
+
+
         <BrowserRouter >
           <div>
             <Sidebar>
